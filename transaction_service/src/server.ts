09ae78432ba0ff7e4b259702a193312356ff5adc
Christian <bernhardt-christian@web.de>
2022-12-07 16:32:05 +0100

    Add Cors

--- transaction_service/src/server.ts
+++ transaction_service/src/server.ts
@@ -1,14 +1,23 @@
 import express from 'express';
-import { json } from 'body-parser'
-import dotenv from 'dotenv'
+import { json } from 'body-parser';
+import dotenv from 'dotenv';
 import { transaction_router } from './api';
-import { databaseConnection } from './db'
+import { databaseConnection } from './db';
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(json());
+const corsOptions ={
+    origin:['http://localhost:3000'], 
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(transaction_router);
+
+
 
 databaseConnection();
 
