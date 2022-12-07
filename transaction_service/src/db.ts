9f0eb771b6192fb358de260c20a1d6eafbdee13e
Matthias Hummel <ma-hummel@gmx.de>
2022-12-07 14:55:21 +0100

    changed path of env.d.ts

--- transaction_service/src/db.ts
+++ transaction_service/src/db.ts
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', true)
 const databaseConnection = async ()=> {
     await mongoose.connect(process.env.MONGO_URI!);
     console.log('MongoDb Connected');   
