db119337af7411f9aced200950049effba904a18
Bennik2000 <bennik.ko@gmail.com>
2022-12-08 08:30:11 +0100

    Add check if the username or the email is already taken

--- authentication_service/src/data/MongooseUserRepository.ts
+++ authentication_service/src/data/MongooseUserRepository.ts
@@ -1,9 +1,12 @@
 import mongoose, { Schema } from "mongoose";
+import { MongoServerError} from 'mongodb';
 import { User, UserRepository } from "./UserRepository";
 
+const DUPLICATE_KEY_ERROR = 11000;
+
 const userSchema = new Schema<User>({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
     licenseType: { type: String, required: true },
     passwordHash: { type: String, required: true }
 });
@@ -30,7 +33,21 @@ export class MongooseUserRepository implements UserRepository {
         const createdUser = new UserModel(user);
         createdUser._id = new mongoose.Types.ObjectId();
 
-        return await createdUser.save();
+        try {
+            return await createdUser.save();
+        } catch(err: any) {
+            if(!(err instanceof MongoServerError)) {
+                throw err;
+            }
+            
+            if(err.code == DUPLICATE_KEY_ERROR) {
+                // TODO: Specific error message depending if the username or email is duplicate
+                throw "The given username or email is already taken."
+            }
+            else {
+                throw "Failed to create user"
+            }
+        }
     }
 
     async getUserByName(username: string): Promise<User|null> {
