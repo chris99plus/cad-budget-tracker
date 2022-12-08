3328a47c28e4bc5ba80d8b1b79135c4c01e15523
Matthias Hummel <ma-hummel@gmx.de>
2022-12-08 21:49:06 +0100

    changed response

--- transaction_service/src/api.ts
+++ transaction_service/src/api.ts
@@ -13,18 +13,18 @@ router.get('/api/v1/', (req: Request, res: Response) => {
 
 router.get('/api/v1/transactions', (req: Request, res: Response) => {
     Transaction.find({}).then( function(allTransactions) {
-        console.log(allTransactions);
-        res.status(200).send(allTransactions);
+        return res.status(200).send({successful: true, data: allTransactions});
+    }).catch(err => {
+        return res.status(400).send({successful: false, message: err});
     });
 });
 
 router.get('/api/v1/transactions/:transactionId', (req: Request, res: Response) => {
     Transaction.findById(req.params.transactionId).then(transaction => {
-        if(transaction === null) { return res.status(400).send("Transaction not found.");}
-        return res.status(200).send(transaction);
+        if(transaction === null) { return res.status(400).send({successful: false, message: "Transaction not found."});}
+        return res.status(200).send({successful: true, data: transaction});
     }).catch(err => {
-        console.log(err)
-        return res.status(400).send(err);
+        return res.status(400).send({successful: false, message: err});
     });
 });
 
@@ -37,19 +37,19 @@ router.post('/api/v1/transactions', (req: Request, res: Response) => {
     })
     newTransaction.save().then(createdTransaction => {
         createdTransaction._id.toString;
-        return res.status(200).send(createdTransaction);
+        return res.status(200).send({successful: true, data: createdTransaction});
     }).catch(err => {
-        return res.status(400).send(err);
+        return res.status(400).send({successful: false, message: err});
     });
 });
 
 router.delete('/api/v1/transactions/:transactionId', (req: Request, res: Response) => {
     Transaction.findByIdAndDelete(req.params.transactionId).then(transaction => {
-        if(transaction === null) { return res.status(400).send("Transaction not found.");}
-        return res.status(200).send(transaction);
+        if(transaction === null) { return res.status(400).send({successful: false, message: "Transaction not found"});}
+        return res.status(200).send({successful: true, data: transaction});
     }).catch(err => {
         console.log(err)
-        return res.status(400).send(err);
+        return res.status(400).send({successful: false, message: err});
     });
 });
 
