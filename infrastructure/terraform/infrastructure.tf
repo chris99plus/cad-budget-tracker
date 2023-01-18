f18b7db4228609f339ff774d6822c8730d671a06
Bennik2000 <bennik.ko@gmail.com>
2023-01-18 23:51:27 +0100

    Adapt terraform in order to deploy the application inside the azure kubernetes cluster

--- infrastructure/terraform/infrastructure.tf
+++ infrastructure/terraform/infrastructure.tf
@@ -2,6 +2,31 @@ variable "AZURE_SUBSCRIPTION_ID" {
   type = string
 }
 
+variable "REGISTRY_USERNAME" {
+  type = string
+}
+
+variable "REGISTRY_PASSWORD" {
+  type = string
+}
+
+variable "REGISTRY_SERVER" {
+  type = string
+}
+
+
+variable "REGISTRY_EMAIL" {
+  type = string
+}
+
+variable "HETZNER_TOKEN" {
+  type = string
+}
+
+variable "HETZNER_DNS_ZONE_ID" {
+  type = string
+}
+
 
 terraform {
   required_providers {
@@ -9,16 +34,29 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.16.1"
+    }
+
+    hetznerdns = {
+      source = "timohirt/hetznerdns"
+      version = "2.1.0"
+    }
   }
 
+  # Configure terraform to store the state in an azure bucket
   backend "azurerm" {
-    resource_group_name = "budget-tracker-pipeline-rg"
+    resource_group_name  = "budget-tracker-pipeline-rg"
     storage_account_name = "budgettrackerterraform"
     container_name       = "terraform"
     key                  = "prod.terraform.tfstate"
   }
 }
 
+
+# ------------------- Initial Azure infrastructure setup -------------------
 provider "azurerm" {
   features {}
 
@@ -54,12 +92,96 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.client_certificate
-  sensitive = true
+# -------------------- Kubernetes setup --------------------
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.cluster_ca_certificate)
 }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config_raw
-  sensitive = true
+
+resource "kubernetes_namespace" "cad_namespace" {
+  metadata {
+    name = "cad"
+  }
+}
+
+
+resource "kubernetes_secret" "regcred_secret" {
+  metadata {
+    name = "regcred"
+    namespace = "cad"
+  }
+  
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "${var.REGISTRY_SERVER}" = {
+          "username" = var.REGISTRY_USERNAME
+          "password" = var.REGISTRY_PASSWORD
+          "email" = var.REGISTRY_EMAIL
+          "auth"     = base64encode("${var.REGISTRY_USERNAME}:${var.REGISTRY_PASSWORD}")
+        }
+      }
+    })
+  }
+}
+
+
+
+# --------------------- Helm setup to deploy the application itself ---------------------
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.host
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.cluster_ca_certificate)
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.client_key)
+  }
+}
+
+resource "helm_release" "budget-tracker" {
+  name      = "budget-tracker"
+  chart     = "./../charts/budget-tracker"
+  namespace = "cad"
+
+  values = [
+    file("./../budgetTrackerTestVars.yaml")
+  ]
+}
+
+
+resource "helm_release" "ingress-controller" {
+  name      = "cad-ingress-controller"
+  chart     = "nginx-ingress"
+  repository = "https://helm.nginx.com/stable"
+  
+}
+
+
+# Search for the ingress in order to get its ip address
+data "kubernetes_ingress_v1" "ingress" {
+  depends_on = [
+    helm_release.budget-tracker,
+    helm_release.ingress-controller
+  ]
+
+  metadata {
+    name = "budget-tracker"
+    namespace = "cad"
+  }
+}
+
+# --------------------- DNS Setup to create the domain ---------------------
+provider "hetznerdns" {
+  apitoken = var.HETZNER_TOKEN
+}
+
+resource "hetznerdns_record" "hetzner_subdomain" {
+  zone_id = var.HETZNER_DNS_ZONE_ID
+  name    = "cad"
+  value   = data.kubernetes_ingress_v1.ingress.status.0.load_balancer.0.ingress.0.ip
+  type    = "A"
 }
