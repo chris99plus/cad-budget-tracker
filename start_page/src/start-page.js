48e5d7bb1e0708970b1cc185c2453f73a6f77b16
Bennik2000 <bennik.ko@gmail.com>
2023-01-24 12:25:20 +0100

    Improve start page docker

--- start_page/src/start-page.js
+++ start_page/src/start-page.js
@@ -11,28 +11,14 @@ import Grid from '@mui/material/Grid';
 import StarIcon from '@mui/icons-material/StarBorder';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import Container from '@mui/material/Container';
-
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const tiers = [
   {
     title: 'Free',
     price: '0',
-    buttonLink: 'http://localhost:3000/signin?licenceType=free',
+    buttonLink: '/signin?licenceType=free',
     description: [
       '10 users included',
       '2 GB of storage',
@@ -46,7 +32,7 @@ const tiers = [
     title: 'Standart',
     subheader: 'Most popular',
     price: '15',
-    buttonLink: 'http://localhost:3000/signin?licenceType=standard',
+    buttonLink: '/signin?licenceType=standard',
     description: [
       '20 users included',
       '10 GB of storage',
@@ -59,7 +45,7 @@ const tiers = [
   {
     title: 'Enterprise',
     price: '30',
-    buttonLink: 'http://localhost:3000/signin?licenceType=enterprise',
+    buttonLink: '/signin?licenceType=enterprise',
     description: [
       '50 users included',
       '30 GB of storage',
@@ -68,31 +54,6 @@ const tiers = [
     ],
     buttonText: 'SIGN UP',
     buttonVariant: 'outlined',
-  },
-];
-
-const footers = [
-  {
-    title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations'],
-  },
-  {
-    title: 'Features',
-    description: [
-      'Cool stuff',
-      'Random feature',
-      'Team feature',
-      'Developer stuff',
-      'Another one',
-    ],
-  },
-  {
-    title: 'Resources',
-    description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
-  },
-  {
-    title: 'Legal',
-    description: ['Privacy policy', 'Terms of use'],
   },
 ];
 
@@ -112,7 +73,7 @@ function PricingContent() {
             Financel
           </Typography>
          
-          <Button href="http://localhost:3000/login" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+          <Button href="/login" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
             Login
           </Button>
         </Toolbar>
@@ -199,37 +160,6 @@ function PricingContent() {
           ))}
         </Grid>
       </Container>
-      {/* Footer */}
-      <Container
-        maxWidth="md"
-        component="footer"
-        sx={{
-          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
-          mt: 8,
-          py: [3, 6],
-        }}
-      >
-        <Grid container spacing={4} justifyContent="space-evenly">
-          {footers.map((footer) => (
-            <Grid item xs={6} sm={3} key={footer.title}>
-              <Typography variant="h6" color="text.primary" gutterBottom>
-                {footer.title}
-              </Typography>
-              <ul>
-                {footer.description.map((item) => (
-                  <li key={item}>
-                    <Link href="#" variant="subtitle1" color="text.secondary">
-                      {item}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Grid>
-          ))}
-        </Grid>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-      {/* End footer */}
     </React.Fragment>
   );
 }
